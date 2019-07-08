f7fbf89d8f10619abd3cb064644f1b5a5b72d05c
happysad- <happysad-@users.noreply.github.com>
2019-07-08 15:14:09 +0100

    arrival-component: Rendering of basic arrival entry, default props added in case props are not passed.

--- src/components/Arrival.js
+++ src/components/Arrival.js
@@ -4,13 +4,35 @@ class Arrival extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      towards: props.towards,
+      expectedArrival: new Date(props.expectedArrival),
+      platformName: props.platformName
+    };
   }
 
   render() {
     return (<>
-
+      <div className="row" id="arrival">
+        <div className="col" id="arrival-towards">
+          { this.state.towards.toUpperCase() }
+        </div>
+        <div className="col" id="arrival-time">
+          {
+            this.state.expectedArrival
+          }
+        </div>
+      </div>
+      <hr/>
     </>);
   }
 }
+
+Arrival.defaultProps = {
+  towards: 'Check front of train',
+  expectedArrival: new Date(),
+  platformName: ''
+};
 
 export default Arrival;
