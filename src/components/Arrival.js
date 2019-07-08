4f7bea936d645e2ba2d34d26d9713c2c5c8b278b
happysad- <happysad-@users.noreply.github.com>
2019-07-08 15:10:59 +0100

    arrival-component: Outline of a basic renderable component.

---
+++ src/components/Arrival.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Arrival extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<>
+
+    </>);
+  }
+}
+
+export default Arrival;
