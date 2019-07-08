7e31119593c3f8d86dd764851f3ef24aff5928ad
happysad- <happysad-@users.noreply.github.com>
2019-07-08 15:30:04 +0100

    board: Basic component which will be used to generate arrival array and query api.

---
+++ src/components/Board/Board.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Store arrivals in state
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    // TODO: Subscribe to TFL Push API
+  }
+
+  componentWillUnmount() {
+    // TODO: Cancel subscriptions and close connection.
+  }
+
+  // TODO: Render Arrival array
+  render() {
+    return (<>
+
+    </>);
+  }
+}
+
+export default Board;
